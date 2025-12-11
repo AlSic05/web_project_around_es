b66caae6b52abb1de26215fbad528555a5fa029e
AlejandraSichaca <aleja.sichaca@hotmail.com>
2025-12-11 10:56:50 -0500

    Clases Section, PopUp

---
+++ scripts/Section.js
@@ -0,0 +1,19 @@
+class Section {
+  constructor({ items, renderer }, classSelector) {
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(`.${classSelector}`);
+  }
+
+  renderItems() {
+    this._items.forEach((item) => {
+      this._renderer(item);
+    });
+  }
+
+  addItem(element) {
+    this._container.append(element);
+  }
+}
+
+export default Section;
